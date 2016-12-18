49580122b4fc7b2f44e3443530093138c0abb9ea
Lubcho Milushev <lubcho.milushev@gmail.com>
2016-12-18 02:32:17 +0200

    ClassWork02/HomeWork02
    C# 1.1

---
+++ 1000DaysAfterBirth1.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000DaysAfterBirth1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateString = Console.ReadLine();
+            string format = "dd-MM-yyyy";
+
+            DateTime result = DateTime.ParseExact(dateString, format, CultureInfo.InvariantCulture);
+            CultureInfo provider = CultureInfo.InvariantCulture;
+
+            DateTime output = Convert.ToDateTime(result).AddDays(999);
+            Console.WriteLine(output.ToString("dd-MM-yyyy"));
+        }
+    }
+}
---
+++ CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = double.Parse(Console.ReadLine());
+            // T(°F) = T(°C) × 1.8 + 32
+            var f = (c * 1.8) + 32;
+
+            Console.WriteLine(Math.Round(f, 2));
+        }
+    }
+}
